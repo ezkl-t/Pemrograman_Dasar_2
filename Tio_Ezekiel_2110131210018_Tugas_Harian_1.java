9627ee20151ad990b3c7ce1019474436e185c4dc
tioez <tioezekiel7@gmail.com>
2022-09-07 13:22:43 +0800

    pemdas 2

---
+++ Tio_Ezekiel_2110131210018_Tugas_Harian_1.java
@@ -0,0 +1,9 @@
+public class Tio_Ezekiel_2110131210018_Tugas_Harian_1 {
+    public static void main(String[] args) {
+        System.out.println("NIM                 : 2110131210018");
+        System.out.println("Nama                : Tio Ezekiel");
+        System.out.println("Alamat              : Jl. Kayu Tangi 1 Jalur 3 No. 24A, Banjarmasin Utara");
+        System.out.println("Hobi                : Bermain game, membaca komik, nonton film, dsb");
+        System.out.println("Mata Kuliah Dikuasai: Bahasa Inggris dan beberapa matkul coding");
+}
+}
---
+++ coba.java
@@ -0,0 +1,20 @@
+// public class coba{
+//     public static void main(String[] args) {
+//         System.out.println("Halo nama Saya Tio");
+//         System.out.print("Saya seorang mahasiswa dari Program Studi Pendidikan Komputer, FKIP ULM");
+//         System.out.println("Sekarang semester 3");
+//     }
+// }
+
+public class coba {
+    public static void main(String[] args) {
+        System.out.println(" _______________  U N I V E R S I T A S    L A M B U N G   M A N G K U R A T");
+        System.out.println("|               | 2 1 1 0 1 3 1 2 1 0 0 1 8");
+        System.out.println("|               | N a m a");
+        System.out.println("|               | T I O   E Z E K I E L");
+        System.out.println("|               | T e m p a t  / T a n g g a l  L a h i r");
+        System.out.println("|               | K U A L A  K A P U A S,   1   F e b r u a r i  2 0 0 3 ");
+        System.out.println("|               | A l a m a t");
+        System.out.println("|_______________| JL. S. P A R M A N   N O. 9 0     K A B. K A P U A S, 7 3 5 1 3");
+    }
+}
